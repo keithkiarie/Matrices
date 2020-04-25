1ca85b39ebc044c19eb1d9982fec3dba91012e46
Keithkiarie <keithkiarie@gmail.com>
2020-04-25 17:33:27 +0300

    Produce matrix from data in DOM

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function CreateMatrixTable() {
     let a = parseInt(document.getElementById("dimension_a").value);
     let b = parseInt(document.getElementById("dimension_b").value);
     let matrix_body = "";
-    let i = 1;
+    let i = 0;
 
     for (let row = 0; row < a; row++) {
         //columns
@@ -36,7 +36,7 @@ function CreateMatrixTable() {
 
         for (let column = 0; column < b; column++) {
             //rows
-            matrix_body += `<input type='number' id='td${i}' style='width:5%' value='0'>`
+            matrix_body += `<input type='number' id='td${i}' style='width:5%' value='0' onchange='InputChange()'>`;
             i++;
         }
     }
@@ -45,9 +45,33 @@ function CreateMatrixTable() {
 };
 CreateMatrixTable();
 
+//get data from DOM and make a multidimensional array
+function ProduceMatrix() {
+
+    let DimensionA = document.getElementById('dimension_a').value;
+    let DimensionB = document.getElementById('dimension_b').value;
+
+    if (DimensionA != DimensionB) return [];
+
+    let Matrix = [];
+    for (let i = 0; i < DimensionA; i++) {
+        Matrix.push([]);
+    }
+
+    for (let i = 0; i < DimensionA; i++) {
+        for (let j = 0; j < DimensionA; j++) {
+            Matrix[i].push(parseInt(document.getElementById(`td${(j * DimensionA) + i}`).value));
+        }
+    }
+
+    return Matrix;
+}
+
+function InputChange() {
+    
+}
+
 let Arr = [[7, 5, 2], [6, 4, 7], [3, 1, 6]];
 let Arr1 = [[4, 7], [1, 6]];
 let Arr2 = [[5, 2], [1, 6]];
 let Arr3 = [[5, 2], [4, 7]];
-
-//alert(GetDeterminant(Arr));
