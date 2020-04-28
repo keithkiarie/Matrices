4b73cb8b652b0de2ae7b5a36a8695a6c0389097c
Keithkiarie <keithkiarie@gmail.com>
2020-04-28 16:02:49 +0300

    Function to calculate inverse of a matrix

--- calculator.js
+++ calculator.js
@@ -1,3 +1,29 @@
+function CreateSubMatrix(row, col, Matrix) {
+    let SubMatrix = [];
+    Matrix.forEach(item => {
+        SubMatrix.push(item.slice());
+    });
+
+    SubMatrix.splice(col, 1); //remove a column
+
+    for (let i = 0; i < SubMatrix.length; i++) {
+        SubMatrix[i].splice(row, 1); //remove items in a row
+    }
+
+    return SubMatrix;
+}
+
+function GetAdjoint(Matrix) {
+    let Adjoint = [];
+    for (let i = 0; i < Matrix.length; i++) Adjoint.push([]);
+
+    for (let i = 0; i < Matrix.length; i++) {
+        for (let j = 0; j < Matrix.length; j++) Adjoint[i][j] = Matrix[j][i];
+    }
+
+    return Adjoint;
+}
+
 function GetDeterminant(Matrix) {
     if (Matrix.length == 2) {
         return ((Matrix[0][0] * Matrix[1][1]) - (Matrix[0][1] * Matrix[1][0]));
@@ -25,17 +51,45 @@ function GetDeterminant(Matrix) {
     return Determinant;
 }
 
-function CreateSubMatrix(row, col, Matrix) {
-    let SubMatrix = [];
-    Matrix.forEach(item => {
-        SubMatrix.push(item.slice());
-    });
+function GetInverse(Matrix) {
 
-    SubMatrix.splice(col, 1); //remove a column
+    let Determinant = GetDeterminant(Matrix);
+    
+    let Inverse = [];
+    for (let i = 0; i < Matrix.length; i++) Inverse.push([]);
+    
 
-    for (let i = 0; i < SubMatrix.length; i++) {
-        SubMatrix[i].splice(row, 1); //remove items in a row
+    if (Matrix.length == 2) {
+        
+        let Adjoint = [[Matrix[1][1], -Matrix[0][1]], [-Matrix[1][0], Matrix[0][0]]];
+        
+        for (let i = 0; i < Adjoint.length; i++) {
+            for (let j = 0; j < Adjoint.length; j++) Inverse[i][j] = Adjoint[i][j] / Determinant;
+        }
+        return Inverse;
     }
 
-    return SubMatrix;
+    let MatrixOfMinors = [];
+    for (let i = 0; i < Matrix.length; i++) MatrixOfMinors.push([]);
+
+    let Operator = "+";
+
+    for (let i = 0; i < Matrix.length; i++) {
+        for (let j = 0; j < Matrix.length; j++) {
+            if (Operator == "+") {
+                MatrixOfMinors[i][j] = GetDeterminant(CreateSubMatrix(i, j, Matrix));
+                Operator = "-";
+            } else {
+                MatrixOfMinors[i][j] = -GetDeterminant(CreateSubMatrix(i, j, Matrix));
+                Operator = "+";
+            }
+        }
+
+        let Adjoint = GetAdjoint(MatrixOfMinors);
+
+        for (let i = 0; i < MatrixOfMinors.length; i++) {
+            for (let j = 0; j < MatrixOfMinors.length; j++) Inverse[i][j] = MatrixOfMinors[i][j] / Determinant;
+        }
+    }
+    return Inverse;
 }
